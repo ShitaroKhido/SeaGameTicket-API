7bd5da24726104ad7653d0763f6bc7b019a3bc1c
ShitaroKhido <khidpagna01@gmail.com>
2023-05-16 16:33:27 +0700

    initial commit

---
+++ app/Http/Controllers/PlaygroundsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Playgrounds;
+use Illuminate\Http\Request;
+
+class PlaygroundsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Playgrounds $playgrounds)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Playgrounds $playgrounds)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Playgrounds $playgrounds)
+    {
+        //
+    }
+}
